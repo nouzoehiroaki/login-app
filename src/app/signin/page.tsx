c0dd5402b46ce346d3cab85b2253d0db5f1972dc
農添広晃 <87853196+nouzoehiroaki@users.noreply.github.com>
2023-09-11 01:18:09 +0900

    ログイン 新規 エラーメッセージ

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 import NextLink from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import styles from '@/styles/signup.module.scss'
-import { signInWithEmail } from '@/lib/firebase/apis/auth'
+import { signInWithEmail,FirebaseResult } from '@/lib/firebase/apis/auth'
+
 
 type formInputs = {
     email: string
@@ -14,18 +16,26 @@ type formInputs = {
  * @description ユーザのサインインを行う画面
  */
 export default function SignInScreen() {
-    const { handleSubmit, register } = useForm<formInputs>()
+    const router = useRouter()
+    const { 
+        handleSubmit, 
+        register,
+        formState: { errors, isSubmitting } 
+    } = useForm<formInputs>()
     const [show, setShow] = useState<boolean>(false)
+    const [notification, setNotification] = useState<{ message: string, status: 'success' | 'error' | null }>({ message: '', status: null });
+    
     const onSubmit = handleSubmit(async (data) => {
-        signInWithEmail({ email: data.email, password: data.password }).then(
-            (res: boolean) => {
-              if (res) {
-                console.log('ログイン成功')
-              } else {
-                console.log('ログイン失敗')
-              }
+        await signInWithEmail({ 
+            email: data.email, 
+            password: data.password 
+        }).then((res: FirebaseResult) => {
+            if (res.isSuccess) {
+                setNotification({ message: res.message, status: 'success' });
+            } else {
+                setNotification({ message: res.message, status: 'error' });
             }
-        )
+        })
     })
     return(
         <div className={styles.flexContainer}>
@@ -33,18 +43,51 @@ export default function SignInScreen() {
                 <h1>ログイン</h1>
                 <form onSubmit={onSubmit}>
                     <div className={styles.vstackInner}>
-                        <div className={styles.formControl}>
+                        <div className={errors.email ? `${styles.formControl} ${styles.invalid}` : styles.formControl}>
                             <label htmlFor='email'>メールアドレス</label>
-                            <input id='email' {...register('email')} />
+                            <input 
+                                id='email'
+                                {...register('email', {
+                                    required: '必須項目です',
+                                    maxLength: {
+                                        value: 50,
+                                        message: '50文字以内で入力してください',
+                                    },
+                                })}
+                            />
+                            {errors.email && 
+                                <span className={styles.errorMessage}>
+                                    {errors.email.message}
+                                </span>
+                            }
+
                         </div>
-                        <div className={styles.formControl}>
+                        <div className={errors.password ? `${styles.formControl} ${styles.invalid}` : styles.formControl}>
                             <label htmlFor='password'>パスワード</label>
                             <div className={styles.inputGroup}>
-                                <input type={show ? 'text' : 'password'} {...register('password')} />
+                                <input 
+                                    type={show ? 'text' : 'password'}
+                                    {...register('password', {
+                                        required: '必須項目です',
+                                        minLength: {
+                                            value: 8,
+                                            message: '8文字以上で入力してください',
+                                        },
+                                        maxLength: {
+                                            value: 50,
+                                            message: '50文字以内で入力してください',
+                                        },
+                                    })}
+                                 />
                                 <button onClick={() => setShow(!show)}>
                                 {show ? 'Hide' : 'Show'}
                                 </button>
                             </div>
+                            {errors.password && 
+                                <span className={styles.errorMessage}>
+                                    {errors.password.message}
+                                </span>
+                            }
                         </div>
                         <button className={styles.loginBtn}>ログイン</button>
                         <NextLink href='/signup' className={styles.signupLink}>
@@ -52,6 +95,11 @@ export default function SignInScreen() {
                         </NextLink>
                     </div>
                 </form>
+                {notification.status && (
+                    <div className={`notification ${notification.status}`}>
+                        {notification.message}
+                    </div>
+                )}
             </div>
         </div>
     )
--- src/lib/firebase/apis/auth.ts
+++ src/lib/firebase/apis/auth.ts
@@ -1,18 +1,41 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from 'firebase/auth'
+import { 
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut
+} from 'firebase/auth'
 import { doc, setDoc } from 'firebase/firestore';
 import { auth,db } from '@/lib/firebase/config'
+
+/** firebaseの処理結果 */
+export type FirebaseResult = {
+    isSuccess: boolean
+    message: string
+}
+
+/** firebaseのエラー */
+type FirebaseError = {
+    code: string
+    message: string
+    name: string
+}
+
+const isFirebaseError = (e: Error): e is FirebaseError => {
+    return 'code' in e && 'message' in e
+}
 
 /**
  * EmailとPasswordでサインイン
  * @param email
  * @param password
- * @returns Promise<boolean>
+ * @returns Promise<FirebaseResult>
  */
 export const signInWithEmail = async (args: {
     email: string
     password: string
-}) : Promise<boolean> => {
-    let result: boolean = false
+}) : Promise<FirebaseResult> => {
+    let result: FirebaseResult = { isSuccess: false, message: '' }
     try {
         const user = await signInWithEmailAndPassword(
             auth,
@@ -20,11 +43,24 @@ export const signInWithEmail = async (args: {
             args.password
         )
         if (user) {
-            result = true
+            result = { isSuccess: true, message: 'ログインに成功しました' }
         }
     } catch (error) {
-        result = false
-        console.log(error)
+        if (
+            error instanceof Error &&
+            isFirebaseError(error) &&
+            error.code === 'auth/user-not-found'
+        ){
+            result = { isSuccess: false, message: 'ユーザが見つかりませんでした' }
+        }else if (
+            error instanceof Error &&
+            isFirebaseError(error) &&
+            error.code === 'auth/wrong-password'
+        ){
+            result = { isSuccess: false, message: 'パスワードが間違っています' }
+        }else {
+            result = { isSuccess: false, message: 'ログインに失敗しました' }
+        }
     }
     return result
 }
@@ -34,55 +70,54 @@ export const signInWithEmail = async (args: {
  * @param username
  * @param email
  * @param password
- * @returns Promise<boolean>
+ * @returns Promise<FirebaseResult>
  */
 
 export const signUpWithEmail = async (args: {
     email: string
     password: string
-}): Promise<boolean> => {
-    let result: boolean = false
+}): Promise<FirebaseResult> => {
+    let result: FirebaseResult = { isSuccess: false, message: '' }
     try {
         const user = await createUserWithEmailAndPassword(
             auth,
             args.email,
             args.password
-        ).then(async (userCredential) => {
-            /** thenから追加します */
-            /** cloud firestoreのコレクション */
-            const colRef = doc(db, 'users', userCredential.user.uid)
-            /** document追加 */
-            await setDoc(colRef, {
-                uid: userCredential.user.uid,
-                email: userCredential.user.email,
-            })
-            return userCredential.user
-        })
+        )
         if (user) {
-            result = true
+            result = { isSuccess: true, message: '新規登録に成功しました' }
         }
     } catch (error) {
-        result = false
-        console.log(error)
+        if (
+            error instanceof Error &&
+            isFirebaseError(error) &&
+            error.code === 'auth/email-already-in-use'
+        ){
+            result = {
+                isSuccess: false,
+                message: 'メールアドレスが既に使用されています',
+            }
+        } else {
+            result = { isSuccess: false, message: '新規登録に失敗しました' }
+        }
     }
     return result
 }
 
 /**
  * ログアウト処理
- * @returns Promise<boolean>
+ * @returns Promise<FirebaseResult>
  */
 
-export const logout = async (): Promise<boolean> => {
-    let result: boolean = false
-  
+export const logout = async (): Promise<FirebaseResult> => {
+    let result: FirebaseResult = { isSuccess: false, message: '' }
+    
     await signOut(auth)
         .then(() => {
-            result = true
+            result = { isSuccess: true, message: 'ログアウトしました' }
         })
         .catch((error) => {
-            console.log(error)
-            result = false
+            result = { isSuccess: false, message: error.message }
         })
     return result
 }
