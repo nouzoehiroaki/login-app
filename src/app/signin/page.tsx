3f4cee1a95c094fa7639b96cac9a64877030573a
農添広晃 <87853196+nouzoehiroaki@users.noreply.github.com>
2023-09-10 20:33:32 +0900

    新規登録 ログインページ

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import NextLink from 'next/link'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import styles from '@/styles/signup.module.scss'
+
+type formInputs = {
+    email: string
+    password: string
+}
+/** サインイン画面
+ * @screenname SignInScreen
+ * @description ユーザのサインインを行う画面
+ */
+export default function SignInScreen() {
+    const { handleSubmit, register } = useForm<formInputs>()
+    const [show, setShow] = useState<boolean>(false)
+    const onSubmit = handleSubmit(async (data) => {})
+    return(
+        <div className={styles.flexContainer}>
+            <div className={styles.vstack}>
+                <h1>ログイン</h1>
+                <form onSubmit={onSubmit}>
+                    <div className={styles.vstackInner}>
+                        <div className={styles.formControl}>
+                            <label htmlFor='email'>メールアドレス</label>
+                            <input id='email' {...register('email')} />
+                        </div>
+                        <div className={styles.formControl}>
+                            <label htmlFor='password'>パスワード</label>
+                            <div className={styles.inputGroup}>
+                                <input type={show ? 'text' : 'password'} {...register('password')} />
+                                <button onClick={() => setShow(!show)}>
+                                {show ? 'Hide' : 'Show'}
+                                </button>
+                            </div>
+                        </div>
+                        <button className={styles.loginBtn}>ログイン</button>
+                        <NextLink href='/signup' className={styles.signupLink}>
+                            <button className={styles.signupBtn}>新規登録はこちらから</button>
+                        </NextLink>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,69 @@
+'use client'
+import NextLink from 'next/link'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import styles from '@/styles/signup.module.scss'
+
+type formInputs = {
+    email: string
+    password: string
+    confirm: string
+}
+/** サインイン画面
+ * @screenname SignInScreen
+ * @description ユーザのサインインを行う画面
+ */
+export default function SignUpScreen() {
+    const { handleSubmit, register } = useForm<formInputs>()
+    const [password, setPassword] = useState(false)
+    const [confirm, setConfirm] = useState(false)
+    const onSubmit = handleSubmit(async (data) => {})
+    const passwordClick = () => setPassword(!password)
+    const confirmClick = () => setConfirm(!confirm)
+
+    return(
+        <div className={styles.flexContainer}>
+            <div className={styles.vstack}>
+                <h1>新規登録</h1>
+                <form onSubmit={onSubmit}>
+                    <div className={styles.vstackInner}>
+                        <div className={styles.formControl}>
+                            <label htmlFor='email'>メールアドレス</label>
+                            <input id='email' {...register('email')} />
+                        </div>
+                        <div className={styles.formControl}>
+                            <label htmlFor='password'>パスワード</label>
+                            <div className={styles.inputGroup}>
+                                <input
+                                    type={password ? 'text' : 'password'}
+                                    {...register('password')}
+                                />
+                                <button onClick={passwordClick}>
+                                    {password ? 'Hide' : 'Show'}
+                                </button>
+                            </div>
+                        </div>
+                        <div className={styles.formControl}>
+                            <label htmlFor='confirm'>パスワード確認</label>
+                            <div className={styles.inputGroup}>
+                                <input
+                                    type={confirm ? 'text' : 'password'}
+                                    {...register('confirm')}
+                                />
+                                <button onClick={confirmClick}>
+                                    {confirm ? 'Hide' : 'Show'}
+                                </button>
+                            </div>
+                        </div>
+                        <button className={styles.loginBtn} type='submit'>
+                            新規登録
+                        </button>
+                        <NextLink href='/signin' className={styles.signupLink}>
+                            <button className={styles.signupBtn}>ログインはこちらから</button>
+                        </NextLink>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
